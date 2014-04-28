2a11e8191329439adf1222efe5b8d6f437cecebd
Vitaly Berg <bergusman@gmail.com>
2014-04-28 23:11:03 +0400

    Initial Commit

---
+++ UIKitDynamicsFlappyBird/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIKitDynamicsFlappyBird
+//
+//  Created by Vitaly Berg on 28/04/14.
+//  Copyright (c) 2014 Vitaly Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
